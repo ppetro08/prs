ead682b5761a776ba18ac0a70451bef63aa43a32
Philip Petrosino <ppetro08@gmail.com>
2022-03-07 21:25:19 -0500

    Cleanup

--- prs-ui/apps/sadarr-e2e/src/support/commands.ts
+++ prs-ui/apps/sadarr-e2e/src/support/commands.ts
@@ -1,46 +1,41 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
     authenticate(): void;
   }
 }
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
 
 Cypress.Commands.add('authenticate', () => {
   cy.window()
     .its('store')
     .invoke('dispatch', {
       type: '[Authentication/API] Login Success',
-      userInfo: {
-        name: 'Testing',
+      user: {
+        id: '1',
+        firstName: 'Test',
+        lastName: 'Tester',
+        email: 'test@test.com',
+        emailConfirmed: true,
+        token: 'token',
+        userRoles: [
+          {
+            userId: '1',
+            roleId: '1',
+            role: {
+              id: '1',
+              name: 'Movie_Add',
+            },
+          },
+          {
+            userId: '1',
+            roleId: '2',
+            role: {
+              id: '2',
+              name: 'Movie_Request',
+            },
+          },
+        ],
       },
     });
 });
-
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
--- prs-ui/apps/sadarr/src/app/api/movie-requests.api.service.ts
+++ prs-ui/apps/sadarr/src/app/api/movie-requests.api.service.ts
@@ -3,13 +3,14 @@ import { Observable } from 'rxjs';
 import { PrsApiService } from '../shared/api/prs.api.service';
 import { MovieRequest } from './models/movie-request.model';
 
-// TODO - Implement service
 @Injectable()
 export class MovieRequestsApiService {
   constructor(private prsApiService: PrsApiService) {}
   getAllRequests(): Observable<MovieRequest[]> {
     return this.prsApiService.get<MovieRequest[]>(`MovieRequest`);
   }
-  // getUserRequests(userId: number): Observable<MovieRequest[]> {
-  // }
+
+  getUserRequests(userId: number): Observable<MovieRequest[]> {
+    return this.prsApiService.get<MovieRequest[]>(`MovieRequest/${userId}`);
+  }
 }
